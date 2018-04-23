77d9dc45f43c18170444f27ef3de74f3097f0f4a
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-23 08:35:27 -0400

    Updated from Brython Server: 4/23/2018 8:35:26 AM

--- middleWord.py
+++ middleWord.py
@@ -1,1 +1,10 @@
+#Liam Collins
+#4/23/18
+#middleWord.py
 
+words = input('Enter list of words: ').split(' ')
+
+if len(words)%2 == 0:
+    print(words[(len(words)/2)-1],words[(len(words)/2)])
+else:
+    print(words[len(words)//2])
