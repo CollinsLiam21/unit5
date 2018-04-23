c7975107f3254064ea08cec968bc53e145ec49f3
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-23 08:49:35 -0400

    Updated from Brython Server: 4/23/2018 8:49:34 AM

--- longestWord.py
+++ longestWord.py
@@ -1,1 +1,14 @@
+#Liam Collins
+#4/23/18
+#longestWord.py
+
+words = input('Enter a list of words: ').split(' ')
+
+good = ''
+for item in words:
+    if len(item)>len(good):
+        good = item
+
+print(good)
+
 
