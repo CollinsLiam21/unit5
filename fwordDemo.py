5d346dadc63de379471a7115075e2009ea774db3
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-23 08:22:06 -0400

    Updated from Brython Server: 4/23/2018 8:22:04 AM

--- fwordDemo.py
+++ fwordDemo.py
@@ -1,1 +1,9 @@
+#Liam Collins
+#4/23/18
+#fwordDemo.py - print out only words that have an f
 
+words = input('Type in some words: ').split(' ')
+
+for item in words:
+    if 'f' in item or 'F' in item:
+        print(item)
