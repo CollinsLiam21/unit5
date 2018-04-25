8d35e798713deeee1474c25c92f8123758a73f1f
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-25 08:11:40 -0400

    Update dictionaryDemo.py

--- dictionaryDemo.py
+++ dictionaryDemo.py
@@ -1,1 +1,13 @@
+#Liam Collins
+#4/25/18
+#dictionaryDemo.py - more list practice 
 
+words = ['computer','mortify','dog','firetruck','yes','python','cat']
+
+words.sort()
+
+num = int(input('What number word do you want? '))
+if num<=0 or num>=len(words)+1:
+    print('Invalid Number')
+else:
+    print(words[num-1])
