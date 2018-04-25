a0c1636a1a9c0972cc4e9551767114227ce7346f
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-25 08:36:51 -0400

    Updated from Brython Server: 4/25/2018 8:36:50 AM

--- betterColoredWindow.py
+++ betterColoredWindow.py
@@ -1,1 +1,17 @@
+#Liam Collins
+#4/25/18
+#betterColoredWindow.py
 
+from random import randint
+from ggame import *
+
+red = Color(0xFF0000,1)
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
+
+def mouseClick(event):
+    colors = [red,green,blue]
+    Sprite(RectangleAsset(1200,800,LineStyle(1,colors[randint(0,2)]),colors[randint(0,2)]))
+
+App().listenMouseEvent('click',mouseClick)
+App().run()
