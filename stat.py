b0a742a3c11a930718a178078ba1c949f37f586b
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-04-27 08:50:09 -0400

    Updated from Brython Server: 4/27/2018 8:50:08 AM

--- stat.py
+++ stat.py
@@ -2,9 +2,19 @@
 #4/26/18
 #stat.py
 
-nums = input('Enter some numbers: ').split(' ')
+L = []
+while True:
+    nums = input('Enter a list of numbers, Type q when finished: ')
+    if nums != 'q':
+        L.append(nums)
+    else:
+        break
 
-print('Min: ',min(nums))
-print('Max: ',max(nums))
-#print('Mean: ',sum(nums)/len(nums))
-print(sum(nums))
+for item in L:
+    print(item)
+print('The max is:', min(L))
+print('The min is:', max(L))
+
+
+
+
