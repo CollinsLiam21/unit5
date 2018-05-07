94ede9092e3c41381ec85d0331549ed5bc2e7ed5
CollinsLiam21 <35492820+CollinsLiam21@users.noreply.github.com>
2018-05-07 08:35:05 -0400

    Updated from Brython Server: 5/7/2018 8:35:04 AM

--- quiz5.py
+++ quiz5.py
@@ -1,1 +1,16 @@
+#
 
+
+def penultimate(L):
+    return L[1:]
+
+def plusEquals(L,integer):
+    for item in L:
+        item = item + integer
+
+def smallest(L):
+    small = L[0]
+    for item in L:
+        if item < small:
+            small = item
+    return small
